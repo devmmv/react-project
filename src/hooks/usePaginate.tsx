d2bc19105d895b6356819162fc988e23c4cfdb27
Mikhail <nodeprog@gmail.com>
2024-07-28 15:11:54 +0300

    refactor:change hook usePaginate

--- src/hooks/usePaginate.tsx
+++ src/hooks/usePaginate.tsx
@@ -1,44 +1,32 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { URLSearchParams } from 'url';
-import { DataType, ItemType } from '../types';
-import { URL } from '../constants';
 
-function usePaginate(query: URLSearchParams) {
-  const [data, setData] = useState<DataType>({
+type PaginateType = {
+  nextPage: number;
+  prevPage: number;
+  count: number;
+  maxPage: number;
+  searchQuery: string | null;
+};
+
+function usePaginate(query: URLSearchParams, count: number) {
+  const [data, setData] = useState<PaginateType>({
     nextPage: 0,
     prevPage: 0,
-    items: [],
-    pageQuery: '',
-    searchQuery: localStorage.getItem('query') || '',
-    isLoaded: false,
-    query: query.toString(),
     count: 0,
     maxPage: 1,
+    searchQuery: localStorage.getItem('query'),
   });
-  const urlRef = useRef('');
-  urlRef.current = `${URL}?${query.toString()}`;
-
-  if (localStorage.getItem('query') && !query.toString())
-    urlRef.current = `${URL}?page=1&search=${localStorage.getItem('query')}`;
 
   useEffect(() => {
-    const fetchURL = urlRef.current;
-
-    fetch(fetchURL)
-      .then((res) => res.json())
-      .then(({ results, count }: { results: ItemType[]; count: number }) => {
-        setData({
-          items: results,
-          isLoaded: true,
-          searchQuery: localStorage.getItem('query'),
-          nextPage: Number(query.get('page')) + 1,
-          prevPage: Number(query.get('page')) - 1,
-          count,
-          maxPage: Math.ceil(count / 10),
-        });
-      })
-      .catch((err: Error) => console.error(err));
-  }, [query]);
+    setData({
+      nextPage: Number(query.get('page')) + 1,
+      prevPage: Number(query.get('page')) - 1,
+      count,
+      maxPage: Math.ceil(count / 10),
+      searchQuery: query.get('search'),
+    });
+  }, [query, count]);
 
   return data;
 }
