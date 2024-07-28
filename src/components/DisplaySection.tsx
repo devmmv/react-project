b81b4642d747346c7395adcd731641532d46a78d
Mikhail <nodeprog@gmail.com>
2024-07-28 15:24:31 +0300

    refactor:DisplaySection

--- src/components/DisplaySection.tsx
+++ src/components/DisplaySection.tsx
@@ -1,52 +1,42 @@
 import { Dispatch, useState } from 'react';
 import { ItemType } from '../types';
 import { Outlet, useOutletContext } from 'react-router-dom';
+import ListItems from './ListItems';
 
 type ContextType = {
-  homeWordLink: string;
+  planetNumber: string;
   name: string;
   setIsOpenInfo: Dispatch<React.SetStateAction<boolean>>;
 };
 
 function DisplaySection() {
   const [isOpenInfo, setIsOpenInfo] = useState(false);
-  const [homeWordLink, setHomeWordLink] = useState('');
+  const [planetNumber, setPlanetNumber] = useState('');
   const [name, setName] = useState('');
   const items = useOutletContext<ItemType[]>();
 
+  if (items.length === 0) {
+    return (
+      <div className="loader-text" style={{ margin: '5rem auto' }}>
+        <p>No cards 🕵 </p>
+      </div>
+    );
+  }
   return (
     <>
       <div className="container">
-        <ol className="box">
-          {items.map((item) => (
-            <li
-              className="item"
-              key={item.name}
-              onClick={() => {
-                if (item.name && isOpenInfo) {
-                  setIsOpenInfo(false);
-                } else {
-                  setIsOpenInfo(true);
-                  setHomeWordLink(item.homeworld);
-                  setName(item.name);
-                }
-              }}
-            >
-              <h2>{item.name}</h2>
-              <p className="item-description">
-                Gender:<span>{item.gender}</span>
-                Hair color:<span>{item.hair_color}</span>
-                <br />
-                Eye color:<span>{item.eye_color}</span>
-                Birth year:<span>{item.birth_year}</span>
-              </p>
-            </li>
-          ))}
-        </ol>
+        <ListItems
+          items={items}
+          isOpenInfo={isOpenInfo}
+          setIsOpenInfo={setIsOpenInfo}
+          setName={setName}
+          setPlanetNumber={setPlanetNumber}
+        />
+
         {isOpenInfo && (
           <Outlet
             context={
-              { homeWordLink, name, setIsOpenInfo } satisfies ContextType
+              { planetNumber, name, setIsOpenInfo } satisfies ContextType
             }
           />
         )}
