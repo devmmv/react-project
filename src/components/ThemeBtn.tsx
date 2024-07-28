5e7ef9dd0c12687a6c5216c1957e0d4cd3ba2e13
Mikhail <nodeprog@gmail.com>
2024-07-28 15:14:22 +0300

    feat:add ThemeBtn

---
+++ src/components/ThemeBtn.tsx
@@ -0,0 +1,34 @@
+import { FormEvent } from 'react';
+import UseTheme from '../contexts/theme';
+
+function ThemeBtn() {
+  const { themeMode, setLightTheme, setDarkTheme } = UseTheme();
+
+  function onChangeBtn(e: FormEvent<HTMLInputElement>) {
+    const darkModeStatus = e.currentTarget.checked;
+
+    if (darkModeStatus) {
+      console.log('forom onChangeBtn function');
+      setDarkTheme();
+    } else {
+      setLightTheme();
+    }
+    console.log(themeMode);
+  }
+
+  return (
+    <span className="theme-switch-wrapper">
+      <label className="theme-switch" htmlFor="checkbox">
+        <input
+          type="checkbox"
+          id="checkbox"
+          onChange={onChangeBtn}
+          checked={themeMode === 'dark'}
+        />
+        <div className="slider round"></div>
+      </label>
+    </span>
+  );
+}
+
+export default ThemeBtn;
